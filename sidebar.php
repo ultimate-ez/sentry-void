d505be9b611abbafe174ed1f3cccb7256d18c084
Masashi Ezaki <ultimateez@gmail.com>
2017-02-22 21:22:45 +0900

    [fix]広告用サイドバー未設定時に、ログインユーザー向けサイドバーウィジェットが表示されない不具合を修正

--- sidebar.php
+++ sidebar.php
@@ -1,19 +1,20 @@
+<?php if (is_user_logged_in()) : ?>
+	<aside class="notification for-edit">
+		<?php if (function_exists('wpp_get_views')) {
+			echo "<ul>";
+			echo "<li>Daily: ".wpp_get_views(get_the_ID(), "daily", true)."</li>";
+			echo "<li>Weekly: ".wpp_get_views(get_the_ID(), "weekly", true)."</li>";
+			echo "<li>Monthly: ".wpp_get_views(get_the_ID(), "monthly", true)."</li>";
+			echo "<li>All: ".wpp_get_views(get_the_ID(), "all", true)."</li>";
+			echo "</ul>";
+		} ?>
+		<?php if (is_singular()) {
+			edit_post_link('この記事を編集する', '<div>', '</div>');
+		} ?>
+	</aside>
+<?php endif; ?>
+
 <?php if ( is_active_sidebar( 'se-sidebar-ad' ) ) : ?>
-	<?php if (is_user_logged_in()) : ?>
-		<aside class="notification for-edit">
-			<?php if (function_exists('wpp_get_views')) {
-				echo "<ul>";
-				echo "<li>Daily: ".wpp_get_views(get_the_ID(), "daily", true)."</li>";
-				echo "<li>Weekly: ".wpp_get_views(get_the_ID(), "weekly", true)."</li>";
-				echo "<li>Monthly: ".wpp_get_views(get_the_ID(), "monthly", true)."</li>";
-				echo "<li>All: ".wpp_get_views(get_the_ID(), "all", true)."</li>";
-				echo "</ul>";
-			} ?>
-			<?php if (is_singular()) {
-				edit_post_link('この記事を編集する', '<div>', '</div>');
-			} ?>
-		</aside>
-	<?php endif; ?>
 	<aside class="sidebar-ad">
 		<span class="title">スポンサードリンク</span>
 	<?php dynamic_sidebar( 'se-sidebar-ad' ); ?>
