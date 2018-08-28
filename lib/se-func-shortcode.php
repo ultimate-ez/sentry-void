a8c44d9f4f4bf9baad76764e8ee53e6881a82dba
Masashi Ezaki <ultimateez@gmail.com>
2018-08-29 01:05:10 +0900

    fix #8 外部リンクのブログカードを作る
    ブログカード作成のショートコードを追加。
    SSL接続でエラーが出るサーバーあり。ogp画像は先方サーバ画像の読み込みなので、将来的には対応要？

--- lib/se-func-shortcode.php
+++ lib/se-func-shortcode.php
@@ -92,10 +92,73 @@ function sentry_bubble_handler( $atts, $content = null ){
   }
 }
 
+function sentry_link_handler ( $atts ){
+  extract( shortcode_atts( array(
+    'url' => '',
+  ) ,$atts ) );
+
+  $url = strip_tags($url);
+
+  // TODO: 自サイトのURLならIDで取得
+
+  if ( !$url ) return;
+
+  $ogp = get_transient( 'outlink-'.$url );
+
+  if ( $ogp === false ){
+
+    require_once('ogp/Parser.php');
+    $content = file_get_contents($url);
+    $ogp = \ogp\Parser::parse($content);
+    
+    if ( !empty($ogp) ){
+      set_transient( 'outlink-'.$url, $ogp, DAY_IN_SECONDS  );
+    }
+  }
+
+  $faviconUrl =  "http://www.google.com/s2/favicons?domain=". $url;
+  $parse_url = parse_url( $url );
+
+  if ( empty($ogp) ){
+    return "<div class='outlink is-simple'><a href='" . $url . "' target='_blank' />" . $url . "</a></div>";
+  } else {
+    $outHTML ="";
+  
+    $outHTML = '<div class="outlink is-blogcard">';
+    $outHTML .= '<a href="' . $ogp['og']['og:url'] . '" rel="bookmark" title="' . $ogp['og']['og:title'] . '">';
+    $outHTML .= '<div class="blogcard-content">';
+    $outHTML .= '<figure class="thumbnail">';
+    if ( $ogp['og']['og:image'] !== "" ){
+      $outHTML .= '<img src="' . $ogp['og']['og:image'] . '"/>';
+    }else{
+      $outHTML .= '<img src="' . get_default_image_uri(300). '" alt="no image" title="no image" width="300" height="300" />';
+    }
+    $outHTML .= '</figure>';
+    $outHTML .= '<div class="blogcard-body">';
+    $outHTML .= '<div class="blogcard-title">' . $ogp['og']['og:title'] . '</div>';
+    $outHTML .= '<div class="blogcard-description">' . mb_strimwidth( $ogp['og']['og:description'], 0, 160) . '...</div>';
+    $outHTML .= '</div>'; // .blogcard-body
+    $outHTML .= '</div>'; // .blogcard-content
+    $outHTML .= '<div class="blogcard-sitename">';
+    $outHTML .= '<img src="'. $faviconUrl .'" />';
+    if ( $ogp['og']['og:site_name'] ){
+      $outHTML .= $ogp['og']['og:site_name'];
+    } else {
+      $parse_url['scheme'] . '://' . $parse_url['host'];
+    }
+    $outHTML .= '</div>'; // .blogcard-sitename
+    $outHTML .= '</a>';
+    $outHTML .= '</div>';
+
+    return $outHTML;
+  }
+}
+
 add_shortcode( 'marker', 'marker_handler' );
 add_shortcode( 'font', 'font_handler' );
 add_shortcode( 'post', 'post_handler' );
 add_shortcode( 'notification', 'notification_handler');
 add_shortcode( 'ad', 'sentry_ad_handler' );
 add_shortcode( 'bubble', 'sentry_bubble_handler');
+add_shortcode( 'link', 'sentry_link_handler');
 ?>
